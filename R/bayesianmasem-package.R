7975ac9cb89a1a1773ccfa037533543e25f2ddd8
James Uanhoro <jamesuanhoro@gmail.com>
2023-10-23 10:08:51 -0500

    base rstan

---
+++ R/bayesianmasem-package.R
@@ -0,0 +1,16 @@
+#' The 'bayesianmasem' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name bayesianmasem-package
+#' @aliases bayesianmasem
+#' @useDynLib bayesianmasem, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2023). RStan: the R interface to Stan. R package version 2.21.8. https://mc-stan.org
+#'
+NULL
