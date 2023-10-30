08b628cf88fa6fc225f0dcb8355a750a1336ce9a
James Uanhoro <jamesuanhoro@gmail.com>
2023-10-30 16:23:13 -0500

    testing added

---
+++ tests/testthat/setup.R
@@ -0,0 +1,40 @@
+bmasem_test_hist <- function(fit) {
+  testthat::expect_error(
+    gg <- bmasem_plot(fit, type = "hist"),
+    NA
+  )
+  testthat::expect_true(inherits(gg, "ggplot"))
+}
+
+bmasem_test_trace <- function(fit) {
+  testthat::expect_error(
+    gg <- bmasem_plot(fit, type = "trace"),
+    NA
+  )
+  testthat::expect_true(inherits(gg, "ggplot"))
+}
+
+bmasem_test_pp_shared <- function(
+    print_out, method, simple = TRUE, error = FALSE) {
+  testthat::expect_true(grepl(method, print_out, ignore.case = TRUE))
+  testthat::expect_true(grepl("Goodness of fit", print_out, ignore.case = TRUE))
+  testthat::expect_true(grepl("PPP", print_out, ignore.case = TRUE))
+  testthat::expect_true(grepl("RMSE", print_out, ignore.case = TRUE))
+  testthat::expect_true(grepl("RMSEA", print_out, ignore.case = TRUE))
+  testthat::expect_true(
+    grepl("Inter-factor correlations", print_out, ignore.case = TRUE)
+  )
+  testthat::expect_true(grepl("Factor loadings", print_out, ignore.case = TRUE))
+  if (isFALSE(simple)) {
+    testthat::expect_true(grepl("Location", print_out, ignore.case = TRUE))
+    testthat::expect_true(grepl("Dispersion", print_out, ignore.case = TRUE))
+    testthat::expect_true(grepl("convergence", print_out, ignore.case = TRUE))
+    testthat::expect_true(grepl("median", print_out, ignore.case = TRUE))
+    testthat::expect_true(grepl("mad", print_out, ignore.case = TRUE))
+  }
+  if (isTRUE(error)) {
+    testthat::expect_true(
+      grepl("Error correlations", print_out, ignore.case = TRUE)
+    )
+  }
+}
---
+++ tests/testthat/test-bmasem_norton.R
@@ -0,0 +1,37 @@
+test_that("Dependent samples works for meta-CFA on issp89", {
+  method <- sample(.method_hash(), 1)
+  model_syntax <- paste0(
+    "distress =~ ", paste0("x", 1:14, collapse = " + "), "\n",
+    "anxiety =~ ", paste0("x", seq(1, 14, 2), collapse = " + "), "\n",
+    "depression =~ ", paste0("x", seq(2, 14, 2), collapse = " + ")
+  )
+  cluster_names <- gsub(
+    ",", "", lapply(strsplit(names(Norton13$data), " "), "[[", 1)
+  )
+  cluster_ids <- as.integer(as.factor(cluster_names))
+  expect_error(fit <- bmasem(
+    model_syntax,
+    sample_cov = Norton13$data, sample_nobs = Norton13$n,
+    cluster = cluster_ids,
+    simple_struc = FALSE, orthogonal = TRUE,
+    warmup = 500, sampling = 500, chains = 1,
+    method = method, type = "dep",
+    refresh = 0, show_messages = FALSE
+  ), NA)
+  expect_true(all(slotNames(fit) %in% c(
+    "major_parameters", "minor_factor_matrix", "data_list",
+    "priors", "stan_fit", "version"
+  )))
+  bmasem_test_hist(fit)
+  bmasem_test_trace(fit)
+  expect_error(
+    print_out <- capture_output(pp_summary(fit), width = 300),
+    NA
+  )
+  bmasem_test_pp_shared(print_out, method)
+  expect_error(
+    print_out <- capture_output(pp_summary(fit, simple = FALSE), width = 300),
+    NA
+  )
+  bmasem_test_pp_shared(print_out, method, FALSE)
+})
