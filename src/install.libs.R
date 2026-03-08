7d9d1f27b707a64ca1e257629cea2f2b1a6929a0
James Uanhoro <James.Uanhoro@gmail.com>
2026-03-07 19:13:38 -0600

    instantiate cleanup

--- src/install.libs.R
+++ src/install.libs.R
@@ -27,7 +27,8 @@ fs::dir_copy(path = "stan", new_path = bin_stan)
 callr::r(
   func = function(bin_stan) {
     instantiate::stan_package_compile(
-      models = instantiate::stan_package_model_files(path = bin_stan)
+      models = instantiate::stan_package_model_files(path = bin_stan),
+      stanc_options = list("O1")
     )
   },
   args = list(bin_stan = bin_stan),
